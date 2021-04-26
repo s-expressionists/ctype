1a4629b6d69f57ecfc905548d4c4846beb46d671
Bike <aeshtaer@gmail.com>
2021-04-26 10:00:34 -0400

    Export simplicitic accessors (wrt #1)

--- packages.lisp
+++ packages.lisp
@@ -3,4 +3,23 @@
   (:export #:specifier-ctype)
   (:export #:ctypep #:subctypep #:ctype=)
   (:export #:disjointp #:negate #:conjoin/2 #:disjoin/2 #:subtract #:unparse
-           #:conjoin #:disjoin))
+           #:conjoin #:disjoin)
+  ;; Interface to interrogate information about types.
+  ;; EXPERIMENTAL, SUBJECT TO CHANGE.
+  (:export #:cclass #:cclass-class
+           #:negation #:negation-ctype
+           #:conjunction #:disjunction #:junction-ctypes
+           #:ccons #:ccons-car #:ccons-cdr
+           #:range #:range-kind #:range-low #:range-high
+           #:range-low-exclusive-p #:range-high-exclusive-p
+           #:fpzero #:fpzero-kind #:fpzero-zero
+           #:ccomplex #:ccomplex-ucpt
+           #:cmember #:cmember-members
+           #:carray #:carray-simplicity #:carray-uaet #:carray-dims
+           #:charset #:charset-pairs
+           #:cvalues #:cvalues-required #:cvalues-optional #:cvalues-rest
+           #:lambda-list #:lambda-list-required #:lambda-list-optional
+           #:lambda-list-rest #:lambda-list-keyp #:lambda-list-key
+           #:lambda-list-aokp
+           #:cfunction #:cfunction-lambda-list #:cfunction-returns
+           #:csatisfies #:csatisfies-fname))
