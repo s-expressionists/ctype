32c30f14d77d7b4487ff4f3fe98e26a6aeaf811a
Alex Wood <aeshtaer@gmail.com>
2024-06-28 15:27:46 -0400

    Classes are not disjoint from their subclasses
    Since classes are subclasses of themselves, this allows us to
    resolve questions like (disjointp 'symbol 'symbol) => NIL T,
    which in turn allows (subtypep 'symbol '(not symbol)) => NIL T.

--- cclass.lisp
+++ cclass.lisp
@@ -30,13 +30,19 @@
     structure-object standard-class))
 
 (defmethod disjointp (client (ct1 cclass) (ct2 cclass))
-  ;; Pick off cases defined by 4.2.2.
-  (flet ((scp (c1 cn2) (subclassp client c1 (find-class client cn2))))
-    (let ((supct1 (find (cclass-class ct1) *disjoint-classes* :test #'scp))
-          (supct2 (find (cclass-class ct2) *disjoint-classes* :test #'scp)))
-      (if (and supct1 supct2 (not (eq supct1 supct2)))
-          (values t t)
-          (values nil nil)))))
+  ;; Classes aren't disjoint to themselves, or to subclasses.
+  (cond
+    ((subclassp client (cclass-class ct1) (cclass-class ct2))
+     (values nil t))
+    ((subclassp client (cclass-class ct2) (cclass-class ct1))
+     (values nil t))
+    (t ;; Pick off cases defined by 4.2.2.
+     (flet ((scp (c1 cn2) (subclassp client c1 (find-class client cn2))))
+       (let ((supct1 (find (cclass-class ct1) *disjoint-classes* :test #'scp))
+             (supct2 (find (cclass-class ct2) *disjoint-classes* :test #'scp)))
+         (if (and supct1 supct2 (not (eq supct1 supct2)))
+             (values t t)
+             (values nil nil)))))))
 
 (defmethod conjoin/2 (client (ct1 cclass) (ct2 cclass))
   (let ((c1 (cclass-class ct1)) (c2 (cclass-class ct2)))
